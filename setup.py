2f236946b60e36bf9c8a3610be7f1c90013148ca
Dave Murphy <dave@schwuk.com>
2013-09-27 09:56:47 +0100

    Officially supporting Python 2.7 and 3.3.

--- setup.py
+++ setup.py
@@ -14,5 +14,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
     ]
 )
